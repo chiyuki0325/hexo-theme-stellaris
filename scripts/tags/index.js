a43f4fc43640782790728dd1880ed7dce17be4c6
Taranaki <55310629+taranakineko@users.noreply.github.com>
2023-12-23 11:44:46 +0800

    🔧 [fix] {% video %} 错误使用了 icon 组件

--- scripts/tags/index.js
+++ scripts/tags/index.js
@@ -43,4 +43,4 @@ hexo.extend.tag.register('okr', require('./lib/okr')(hexo), {ends: true})
 // stelaris 新增组件
 hexo.extend.tag.register('bilicard', require('./lib/bilicard')(hexo), {async: true})
 hexo.extend.tag.register('icon', require('./lib/icon')(hexo))
-hexo.extend.tag.register('video', require('./lib/icon')(hexo))
+hexo.extend.tag.register('video', require('./lib/video')(hexo))
