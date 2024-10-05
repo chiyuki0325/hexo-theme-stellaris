6a09da584720ff02a32346e113f5c635bb28fdde
Kirikaze Chiyuki <me@chyk.ink>
2024-10-05 12:23:21 +0800

    🔧 [fix] 修复 wiki 主页不显示 tag 问题

--- layout/components/main/navbar/list_wiki.jsx
+++ layout/components/main/navbar/list_wiki.jsx
@@ -1,30 +1,40 @@
 module.exports = function NavBarListWiki(props) {
-    const {page, config, theme, url_for, __} = props;
-    return (
-        <div className="nav-wrap">
-            <nav className="sub wiki cap">
-                {/* 所有项目 */}
-                <a className={(page.filter === false) ? "active" : ""} href={url_for(config.wiki_dir || "/wiki/")}>
-                    {__("btn.all_wiki")}
-                </a>
-                {/*项目分类*/}
-                {(()=>{
-                    const { shelf, all_tags } = theme.wiki;
-                    const result = [];
-                    for (let id of Object.keys(all_tags)) {
-                        let tag = all_tags[id];
-                        let projects = tag.items.filter(item => shelf.includes(item))
-                        if (projects && projects.length > 0) {
-                            const isActive = (tag.name && tag.name.length > 0 && page.tagName === tag.name);
-                            return result.push(<a className={isActive ? "active" : ""} href={url_for(tag.path)}>
-                                {tag.name}
-                            </a>);
-                        }
-                    }
+  const { page, config, theme, url_for, __ } = props;
+  return (
+    <div className="nav-wrap">
+      <nav className="sub wiki cap">
+        {/* 所有项目 */}
+        <a
+          className={page.filter === false ? "active" : ""}
+          href={url_for(config.wiki_dir || "/wiki/")}
+        >
+          {__("btn.all_wiki")}
+        </a>
+        {/*项目分类*/}
+        {(() => {
+          const { shelf, all_tags } = theme.wiki;
+          const result = [];
 
-                    return result;
-                })()}
-            </nav>
-        </div>
-    )
-}
+          for (let id of Object.keys(all_tags)) {
+            let tag = all_tags[id];
+            let projects = tag.items.filter((item) => shelf.includes(item));
+            if (projects && projects.length > 0) {
+              const isActive =
+                tag.name && tag.name.length > 0 && page.tagName === tag.name;
+              result.push(
+                <a
+                  className={isActive ? "active" : ""}
+                  href={url_for(tag.path)}
+                >
+                  {tag.name}
+                </a>,
+              );
+            }
+          }
+
+          return result;
+        })()}
+      </nav>
+    </div>
+  );
+};
