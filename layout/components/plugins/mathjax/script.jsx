6238ed8a89afc279f2c09f825d4ca61bea26f651
Kirikaze Chiyuki <me@chyk.ink>
2024-12-08 20:38:43 +0800

    🔧 [fix] MathJax 插件加载逻辑

--- layout/components/plugins/mathjax/script.jsx
+++ layout/components/plugins/mathjax/script.jsx
@@ -1,29 +1,44 @@
 const MathJaxScripts = (props) => {
-    const { theme } = props;
-    const { plugins } = theme;
-    const { mathjax } = plugins;
-    const { enabled, cdn } = mathjax;
+  const { theme } = props
+  const { plugins } = theme
+  const { mathjax } = plugins
+  const { enabled, cdn } = mathjax
 
-    
-    if (enabled) {
-        const loadMathScript = `
-            function loadMathJax() {    
-                if (typeof MathJax == "undefined")  // 没有载入脚本就先载入脚本
-                    stellar.loadScript('${cdn}', {defer:true});
-                else // 否则立即渲染
-                    MathJax.typeset();
-            }
-            InstantClick.on('change', () => {
-                loadMathJax() 
-            });
-            window.addEventListener(
-                "load", loadMathJax, false
-            );
-        `;
-        return <script data-no-instant="true" dangerouslySetInnerHTML={{__html: loadMathScript}}/>;
-    } else {
-        return <></>;
-    }
-};
+  if (enabled) {
+    const loadMathScript = `
+        function loadMathJax() {
+        console.log('MathJax enabled')
+            stellar.loadScript('${cdn}', {defer:true}).then(() => {
+                MathJax.Hub.Config({
+                tex2jax: {
+                    inlineMath: [ ['$','$'], ["\\(","\\)"] ],
+                    processEscapes: true
+                }
+                });
+                MathJax.Hub.Config({
+                tex2jax: {
+                    skipTags: ['script', 'noscript', 'style', 'textarea', 'pre', 'code']
+                }
+                });
+                MathJax.Hub.Queue(function() {
+                var all = MathJax.Hub.getAllJax(), i;
+                for(i=0; i < all.length; i += 1) {
+                    all[i].SourceElement().parentNode.className += ' has-jax';
+                }
+                });
+            })
+        }
+        loadMathJax()
+        `
+    // 简单的加载，极致的享受
+    return (
+      <>
+        <script dangerouslySetInnerHTML={{ __html: loadMathScript }} />
+      </>
+    )
+  } else {
+    return <></>
+  }
+}
 
-module.exports = MathJaxScripts;
+module.exports = MathJaxScripts
