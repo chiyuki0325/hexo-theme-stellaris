6adb83cc7907fc59611da2e888a47b3e0dca1c05
Jasmine <75739606+sudoskys@users.noreply.github.com>
2024-12-09 12:42:55 +0800

    🐟 [fix] 更新 mermaid 版本并增加错误处理
    #23

--- layout/components/plugins/mermaid/script.jsx
+++ layout/components/plugins/mermaid/script.jsx
@@ -4,47 +4,66 @@ const MermaidScripts = (props) => {
 
   if (enabled) {
     const loadMermaidScript = `
-      function loadMermaid() {
-        console.log('Mermaid enabled')
-        stellar.loadScript('${js}', {defer:true}).then(() => {
-          mermaid.initialize({
-            startOnLoad: false,
-            theme: "${blogTheme.style.darkmode}".startsWith("auto") ? (
-              window.matchMedia('(prefers-color-scheme: dark)').matches ? "dark" : "${theme}"
-            ) : "${theme}",
-            logLevel: 3,
-            flowchart: {
-              useMaxWidth: false,
-              htmlLabels: true,
-              curve: "linear"
-            },
-            gantt: {
-              axisFormat: "%Y/%m/%d"
-            },
-            sequence: {
-              actorMargin: 50
+async function loadMermaid() {
+  try {
+    console.log("Mermaid enabled");
+
+    // Load Mermaid script
+    await stellar.loadScript("${js}", { defer: true });
+
+    // Initialize Mermaid
+    mermaid.initialize({
+      startOnLoad: false,
+      theme:
+        "${blogTheme.style.darkmode}".startsWith("auto")
+          ? window.matchMedia("(prefers-color-scheme: dark)").matches
+            ? "dark"
+            : "${theme}"
+          : "${theme}",
+      // logLevel: 3,
+      flowchart: {
+        useMaxWidth: false,
+        htmlLabels: true,
+        curve: "linear",
+      },
+      gantt: {
+        axisFormat: "%Y/%m/%d",
+      },
+      sequence: {
+        actorMargin: 50,
+      },
+    });
+
+    // Select the non-processed Mermaid elements
+    const mermaidElements = document.querySelectorAll('pre.mermaid:not([data-processed="true"])') || [];
+    for (const ele of mermaidElements) {
+      try {
+        // Wait for the element to be fully loaded
+        await new Promise((resolve) => {
+          const interval = setInterval(() => {
+            if (ele.innerText.trim() !== "") {
+              clearInterval(interval);
+              resolve();
             }
-          })
+          }, 20);
         });
-        
-        (document.querySelectorAll('pre.mermaid:not([data-processed="true"])') ?? []).forEach(e => {
-          (new Promise(resolve => {
-            const interval = setInterval(() => {
-              if (e.innerText !== "") {
-                clearInterval(interval)
-                resolve(e)
-              }
-            }, 20)
-          })).then(e => {
-            const uniqueId = "mermaidGraph_" + Math.random().toString(36).substr(2, 9)
-            e.setAttribute("data-processed", "true")
-            mermaid.render(uniqueId, e.innerText, svg => {
-              e.innerHTML = svg
-            })
-          })
-        })
+
+        // Render Mermaid Graph
+        const uniqueId = "mermaidGraph_" + Math.random().toString(36).substr(2, 9);
+        ele.setAttribute("data-processed", "true");
+        // print the graph definition
+        // console.log("Rendering Mermaid element:\\n", ele.innerText);
+        const { svg } = await mermaid.render(uniqueId, ele.innerText);
+        ele.innerHTML = svg;
+      } catch (renderErr) {
+        console.error("Error rendering Mermaid element:", renderErr, ele);
       }
-      loadMermaid()
+    }
+  } catch (err) {
+    console.error("Error initializing Mermaid:", err);
+  }
+}
+loadMermaid();
       `
     return <script dangerouslySetInnerHTML={{ __html: loadMermaidScript }} />
   } else {
